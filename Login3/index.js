887f2e9ff46ab4dee16a5b9fa6b835956815089a
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-07-03 14:35:20 -0300

    atualização

---
+++ Login3/index.js
@@ -0,0 +1,11 @@
+
+  function mostrar(){
+  document.getElementById('login1').style.display = 'none';
+  document.getElementById('cadastro1').style.display = 'block';
+  };
+
+  function mostrar1(){
+    document.getElementById('cadastro1').style.display = 'none';
+    document.getElementById('login1').style.display = 'block';
+    };
+  
